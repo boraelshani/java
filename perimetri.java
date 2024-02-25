370b37faaaab55fd8fd6143985ce1a7ee43da7c7
Boraelshani <boraelshani01@gmail.com>
2024-02-25 20:08:41 +0100

    first commit

---
+++ perimetri.java
@@ -0,0 +1,8 @@
+public class Detyra19{
+public static void main (String org[]){
+double width=4.5;
+double height=7.9;
+System.out.println("Perimetri i drejtekendeshit eshte:" +(2*width + 2*height));
+System.out.println("Syprina e drejtekendeshit eshte:" +(width*height));
+ }
+ }
